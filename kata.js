3bfc96ce71582238c0d283c04e7e9718c06b4140
githubmave <lidanmei@yahoo.com>
2020-10-18 23:37:51 +1300

    Update kata.js

--- kata.js
+++ kata.js
@@ -5,12 +5,21 @@
 // getGreeting should return a string containing
 // 'Hello ' and the contents of `name`
 function getGreeting (name) {
-  var 
+
+  
+  return "Hello " +name;
+
+   
 }
 
 // ageOneYear should return a new object with an `age` property 1 greater
 // than the `age` property of `obj`
 function ageOneYear (obj) {
+  
+  var objNew={};
+  objNew.age=obj.age+1;
+
+  return objNew
 }
 
 // makeObject should return an object that looks like this:
@@ -19,40 +28,79 @@ function ageOneYear (obj) {
 //   key: value
 // }
 function makeObject (key, value) {
+     
+    var obj={};
+    obj[key]=value;
+     return obj;  
 }
 
 // getPropertyValue should return the value of the
 // property contained in the `key` of `obj`
 function getPropertyValue (obj, key) {
+    var value=obj.key;
+    return value;
+
 }
 
 // addName should return a copy of `obj` with the addition of a `name`
 // property that has the value of the `name` argument
 // Tip: consider the object literal spread syntax
 function addName (obj, name) {
+    var objNew=obj;
+    objNew.name=name;
+
+    return objNew;
+
 }
 
 // deleteProperty should return a new copy of `obj` without the property name
 // that matches the `key` parameter
 // Tip: consider JavaScript's `delete` operator
 function deleteProperty (obj, key) {
-}
+     var objNew={};
+     delete obj[key];
+     return objNew=obj;
+} 
 
 // returnErrorIfFalsy should return a JavaScript Error object with message:
 //   'Oh no, an error!'
 // if val evaluates to false
 // Tip: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
 function returnErrorIfFalsy (val) {
+
+  try {
+
+  if(val==false)
+    throw new Error('Oh no, an error!')
+  } catch (e) {
+    console.error(e.name + ': ' + e.message)
+  }
 }
 
 // keys should return an array of the object's property names (keys)
 // For example, given { foo: 1, bar: 2 } it would return ['foo', 'bar']
 function getKeys (obj) {
+      var arr=[];
+      for(var key in obj){
+    
+           arr.push(key);
+      }
+
+
+      return arr;
 }
 
 // getValues should return an array of the object's own values
 // For example, given { foo: 1, bar: 2 } it would return [1, 2]
 function getValues (obj) {
+
+     var arr=[];
+     for(var key in obj){
+        arr.push(obj[key]);
+
+     }
+
+      return arr;
 }
 
 /**
@@ -63,28 +111,61 @@ function getValues (obj) {
 // `item`. For example, makeArrayOfItem('foo', 2) would return:
 // ['foo', 'foo']
 function makeArrayOfItem (item, length) {
+     var arr=[];
+     
+     for(var i=0;i<length;i++){
+         arr.push(item);
+     }
+
+    return arr;
 }
 
 // makeArrayOfItems should return an array containing all arguments passed to it
 // Tip: consider JavaScript's Rest parameters
-function makeArrayOfItems () {
-}
+function makeArrayOfItems (...arr) {
+     var arrNew=arr;
+
+    
+      return arrNew;
+} 
 
 // hasItem should return true if `item` is present in `arr` at least once,
 // otherwise it should return false.
 // Tip: there is an array function that makes this straightforward
 function hasItem (arr, item) {
+
+   
+
+    var itemNew=arr.find(item =>{
+         return item;
+    });
+
+    return !(itemNew==undefined);
 }
 
 // getItemAtIndex should return arr[idx] but only if that index exists:
 // if it doesn't, return a JavaScript Error object.
-function getItemAtIndex (arr, idx) {
+function getI temAtIndex (arr, idx) {
+
+  try {
+    
+    var value=arr[idx]
+    if(idx>arr.length-1)
+    throw new Error('Oh error occured')
+  } catch (error) {
+    console.error(error.name +":"+error.message);
+  }
+
+  return value;
 }
 
 // replaceItemAtIndex should return a copy of `arr` with
 // the element at `idx` replaced with `item`
 // Tip: consider the array literal spread syntax
 function replaceItemAtIndex (arr, idx, item) {
+     var arrNew=arr;
+
+
 }
 
 // insertItemAtIndex should return a copy of `arr` with `item` inserted at
